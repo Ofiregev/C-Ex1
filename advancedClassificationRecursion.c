2052b72a5c13c0fa9676e5912dfe6200057b4a99
OFIR REGEV <92684730+Ofiregev@users.noreply.github.com>
2021-11-04 14:43:15 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,8 +1,24 @@
 #include<stdio.h>
 #include "NumClass.h"
+// int isArmstrong(int size,int n,int x,int s);
+// int Count(int n);
+// int powers(int a, int b);
+// int bool(int a,int b);
+// int isPalindrome(int n );
+// int reverse(int num);
+int isArmstrong(int n){
+    int temp =n;
+    if(temp==0)
+    {
+        return 0;
+    }
+    int size=Count(n);
+    int x=n;
+    int w=0;
+    return isArmstrong1(size,n,x,w);
+}
 
-
-int isArmstrongrec(int size,int n,int x,int s){
+int isArmstrong1(int size,int n,int x,int s){
 int b=0;
 int v=n;
 int t;
@@ -14,13 +30,41 @@ if(n==0&&x==s){
 }else{
 t=v%10;
 s=s+powers(t,size);
-return isArmstrongrec(size,v/10,x,s);
+return isArmstrong1(size,v/10,x,s);
 
 }
 }
-int recIsPalindrome(int num)
+
+int Count(int n){
+    int count=0;
+    int i;
+    for(i=n;i>0;i=i/10){
+        count++;
+    }
+    return count;
+}
+int powers(int a,int b){
+int j;
+int s=a;
+for(j=b;j>1;j--){
+    s=s*a;
+
+}
+return s;
+}
+int reverse(int num,int sum)
 {
-    if(num == reverse(num))
+    int rem;
+    if(num!=0){
+            rem=num%10;
+            sum=sum*10+rem;
+            return reverse((num/10),sum);
+    }
+    return sum;
+} 
+int isPalindrome(int num)
+{
+    if(num == reverse(num,0))
     {
         return 1;
     }
@@ -29,16 +73,3 @@ int recIsPalindrome(int num)
 }
 
 
-int reverse(int num)
-{
-    int rem;
-    static int sum=0;
-    if(num!=0){
-            rem=num%10;
-            sum=sum*10+rem;
-            reverse(num/10);
-    }
-        else
-            return sum;
-    return sum;
-} 
